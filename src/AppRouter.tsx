a6ceca4bd1e872d32446d2e886fc917b24266ef8
caokhanh1 <daxxua911@gmail.com>
2024-06-10 13:18:31 +0700

    third

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,9 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/layout";
-import HomePages from "./Pages/HomePages";
-import AuthCallbackPage from "./Pages/AuthCallbackPage";
-import UserProfilePage from "./Pages/UserProfilePage";
+import HomePage from "./pages/HomePages";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
 
 const AppRoutes = () => {
   return (
@@ -12,18 +16,49 @@ const AppRoutes = () => {
         path="/"
         element={
           <Layout showHero>
-            <HomePages />
+            <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
+      <Route
+        path="/detail/:restaurantId"
+        element={
+          <Layout showHero={false}>
+            <DetailPage />
+          </Layout>
+        }
+      />
       <Route element={<ProtectedRoute />}>
+        <Route
+          path="/order-status"
+          element={
+            <Layout>
+              <OrderStatusPage />
+            </Layout>
+          }
+        />
         <Route
           path="/user-profile"
           element={
             <Layout>
               <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
             </Layout>
           }
         />
@@ -33,4 +68,5 @@ const AppRoutes = () => {
     </Routes>
   );
 };
+
 export default AppRoutes;
--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -3,19 +3,33 @@ import { Button } from "./ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function MobileNavLinks() {
-
-    const { logout } = useAuth0();
+  const { logout } = useAuth0();
 
   return (
     <>
+      <Link
+        to="/order-status"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        Order Status
+      </Link>
+      <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        My Restaurant
+      </Link>
       <Link
         to="/user-profile"
         className="flex bg-white items-center font-bold hover:text-orange-500"
       >
         User Profile
       </Link>
- 
-      <Button onClick={()=>logout()} className="flex items-center font-bold px-3 hover:bg-gray-500">
+
+      <Button
+        onClick={() => logout()}
+        className="flex items-center font-bold px-3 hover:bg-gray-500"
+      >
         Log Out
       </Button>
     </>
